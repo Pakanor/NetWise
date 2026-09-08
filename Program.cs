2b37f4e7ce1dd13b60ae07cc618f6ea3a3f8e0de
Robert <robert.p3@o2.pl>
2026-09-08 18:25:48 +0200

    feat: complete application implementation and verify file output

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CatFactFetcher.Configuration;
+using CatFactFetcher.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+    })
+    .ConfigureServices((context, services) =>
+    {
+        services.Configure<CatFactSettings>(
+            context.Configuration.GetSection(CatFactSettings.SectionName));
+
+        services.AddHttpClient<ICatFactService, CatFactService>((serviceProvider, client) =>
+        {
+            var settings = serviceProvider.GetRequiredService<IOptions<CatFactSettings>>().Value;
+            client.BaseAddress = new Uri(settings.ApiBaseUrl);
+            client.Timeout = TimeSpan.FromSeconds(10);
+        });
+
+        services.AddSingleton<IFileWriterService, FileWriterService>();
+    })
+    .Build();
+
+var catFactService = host.Services.GetRequiredService<ICatFactService>();
+var fileWriterService = host.Services.GetRequiredService<IFileWriterService>();
+var settings = host.Services.GetRequiredService<IOptions<CatFactSettings>>().Value;
+
+var factResponse = await catFactService.GetRandomFactAsync();
+
+if (factResponse is not null && !string.IsNullOrWhiteSpace(factResponse.Fact))
+{
+    await fileWriterService.AppendLineAsync(settings.OutputFilePath, factResponse.Fact);
+}
